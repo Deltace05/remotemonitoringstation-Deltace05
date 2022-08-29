457afcb8c4e80fdbd9396a96399c296a0e74ada3
Deltace05 <ben.mackenzie05@gmail.com>
2022-08-29 12:58:47 +1000

    Update RMS.ino

--- RMS/RMS.ino
+++ RMS/RMS.ino
@@ -8,8 +8,24 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
+//Temperature & TFT Screen
+#include <Wire.h>
+#include "Adafruit_ADT7410.h"
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include "Adafruit_miniTFTWing.h"
+
 AsyncWebServer server(80);
 
+Adafruit_miniTFTWing ss;
+#define TFT_RST    -1    // we use the seesaw for resetting to save a pin
+#define TFT_CS   14
+#define TFT_DC   32
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+
+// Create the ADT7410 temperature sensor object
+Adafruit_ADT7410 tempsensor = Adafruit_ADT7410();
 
 // RTC Start - Remove if unnecessary
 #include "RTClib.h"
@@ -25,10 +41,38 @@ boolean LEDOn = false; // State of Built-in LED true=on, false=off.
 
 void setup() {
   Serial.begin(9600);
+  
   while (!Serial) {
     delay(10);
   }
   delay(1000);
+  Serial.println("ADT7410 demo");
+  
+  // Make sure the sensor is found, you can also pass in a different i2c
+  // address with tempsensor.begin(0x49) for example
+  if (!tempsensor.begin()) {
+    Serial.println("Couldn't find ADT7410!");
+    while (1);
+  }
+  
+  if (!ss.begin()) {
+    Serial.println("seesaw init error!");
+    while(1);
+  }
+  else Serial.println("seesaw started");
+
+  ss.tftReset();
+  ss.setBacklight(0x0); //set the backlight fully on
+  
+  // Use this initializer (uncomment) if you're using a 0.96" 180x60 TFT
+  tft.initR(INITR_MINI160x80);   // initialize a ST7735S chip, mini display
+  
+  tft.setRotation(3);
+  
+  tft.fillScreen(ST77XX_BLACK);
+  
+  // sensor takes 250 ms to get first readings
+  delay(250);
 
   if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
     // Follow instructions in README and install
@@ -69,11 +113,9 @@ void setup() {
 void loop() {
 
   builtinLED();
-
-
+  updateTemperature();
   delay(LOOPDELAY); // To allow time to publish new code.
 }
-
 
 void builtinLED() {
   if (LEDOn) {
@@ -81,6 +123,26 @@ void builtinLED() {
   } else {
     digitalWrite(LED_BUILTIN, LOW);
   }
+}
+
+void updateTemperature() {
+  // Read and print out the temperature, then convert to *F
+  float c = tempsensor.readTempC();
+  float f = c * 9.0 / 5.0 + 32;
+  Serial.print("Temp: "); Serial.print(c); Serial.print("*C\t"); 
+  Serial.print(f); Serial.println("*F");
+  String tempInC = String(c);
+  tftDrawText(tempInC, ST77XX_BLUE);
+  delay(1000);
+}
+
+void tftDrawText(String text, uint16_t color) {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 0);
+  tft.setTextSize(3);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
 }
 
 void logEvent(String dataToLog) {
